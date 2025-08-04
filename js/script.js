7f2b720a1062f3ac15d6ca783fe0b4a16e56d2f2
Montaguti <5656679+Montaguti@users.noreply.github.com>
2025-08-04 20:08:58 -0300

    removed card-circuit, add card flipper

--- js/script.js
+++ js/script.js
@@ -3,12 +3,42 @@ if (window.location.protocol === "http:") {
 }
 
 $(document).ready(function() {
+  const flipper = $('.card-flipper');
+  let isFlipping = false;
+  let isFront = true;
+
+  // Click to flip the card
+  flipper.on('click', function() {
+    if (isFlipping) {
+      return;
+    }
+    isFlipping = true;
+
+    if (isFront) {
+      flipper.css('transform', 'rotateY(180deg)');
+    } else {
+      flipper.css('transform', 'rotateY(0deg)');
+    }
+    
+    isFront = !isFront;
+
+    setTimeout(() => {
+      isFlipping = false;
+    }, 800); // Match CSS transition duration
+  });
+
   // Smooth scrolling for anchor links
   $('a[href^="#"]').on('click', function(event) {
     event.preventDefault();
     const target = this.hash;
     const navbarHeight = $('.navbar').outerHeight();
     const margin = 20; // Extra margin from the top
+
+    // Reset card on anchor click
+    if (!isFront) {
+        flipper.css('transform', 'rotateY(0deg)');
+        isFront = true;
+    }
 
     $('html, body').animate({
       scrollTop: $(target).offset().top - navbarHeight - margin
