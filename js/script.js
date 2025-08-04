b2a52d7f1acb143c15935b053be72d5d38d0dc9e
Montaguti <5656679+Montaguti@users.noreply.github.com>
2025-08-04 19:08:28 -0300

    redirecting user to https if it is on http

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+if (window.location.protocol === "http:") {
+  window.location.href = "https:" + window.location.href.substring(5);
+}
+
 $(document).ready(function() {
   // Smooth scrolling for anchor links
   $('a[href^="#"]').on('click', function(event) {
