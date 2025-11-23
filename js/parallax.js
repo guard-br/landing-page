b4082ae454ad96702908ae834615890e62373b17
Montaguti <5656679+Montaguti@users.noreply.github.com>
2025-11-23 18:59:42 -0300

    parallax and riscos's section

--- js/parallax.js
+++ js/parallax.js
@@ -3,4 +3,17 @@ document.addEventListener("scroll", function () {
   const scrollPosition = window.pageYOffset;
 
   heroContent.style.transform = "translateY(" + scrollPosition * 0.5 + "px)";
+
+  const parallaxElements = document.querySelectorAll(".parallax");
+
+  for (const element of parallaxElements) {
+    const elementTop = element.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+
+    if (elementTop < windowHeight) {
+      const elementOffsetTop = element.offsetTop;
+      const yPos = (scrollPosition - elementOffsetTop) * 0.1;
+      element.style.transform = "translateY(" + yPos + "px)";
+    }
+  }
 });
