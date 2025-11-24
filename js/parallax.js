0605b4d29d47eeb94136775fc183ba9da40090e0
Montaguti <5656679+Montaguti@users.noreply.github.com>
2025-11-24 09:24:29 -0300

    removed background images on mobile, add parallax effect for text

--- js/parallax.js
+++ js/parallax.js
@@ -1,22 +1,29 @@
 document.addEventListener("scroll", function () {
   const heroContent = document.querySelector(".hero-content");
+  const ctaContent = document.querySelector(".cta-content");
+  const ctaSection = document.querySelector(".cta-section");
   const scrollPosition = window.pageYOffset;
   const isMobile = window.innerWidth <= 768;
 
   // Parallax for hero content
   heroContent.style.transform = "translateY(" + scrollPosition * 0.3 + "px)";
 
-  // Parallax for CTA on mobile
-  if (isMobile) {
-    const ctaSection = document.querySelector(".cta-section");
-    if (ctaSection) {
-        const elementTop = ctaSection.getBoundingClientRect().top;
-        const windowHeight = window.innerHeight;
+  if (ctaSection) {
+    const elementTop = ctaSection.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
 
-        if (elementTop < windowHeight && elementTop > -ctaSection.offsetHeight) {
-            const yPos = -((scrollPosition - ctaSection.offsetTop) * 0.1);
-            ctaSection.style.backgroundPositionY = yPos + "px";
-        }
+    if (elementTop < windowHeight && elementTop > -ctaSection.offsetHeight) {
+      // Parallax for cta content
+      if (ctaContent) {
+        const yPos = (scrollPosition - ctaSection.offsetTop) * 0.3;
+        ctaContent.style.transform = "translateY(" + yPos + "px)";
+      }
+
+      // Parallax for CTA on mobile
+      if (isMobile) {
+        const yPos = -((scrollPosition - ctaSection.offsetTop) * 0.1);
+        ctaSection.style.backgroundPositionY = yPos + "px";
+      }
     }
   }
 });
